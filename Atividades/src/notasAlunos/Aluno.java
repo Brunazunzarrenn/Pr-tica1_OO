218fc8bc111a84ddb262ef050a78c46444de606d
Bruna Zunzarren <104565697+Brunazunzarrenn@users.noreply.github.com>
2022-09-01 21:54:00 -0300

    Update Aluno.java

--- Atividades/src/notasAlunos/Aluno.java
+++ Atividades/src/notasAlunos/Aluno.java
@@ -3,82 +3,54 @@ package notasAlunos;
 public class Aluno {
 	private String nome;
 	private Integer matricula;
-	private Double notaAV1;
-	private Double notaAV2;
-	private Double notaAE;
+	private float notaAV1;
+	private float notaAV2;
+	private float notaAE;
 	private String curso;
 	private Integer periodo;
 
-	public Aluno(String nome, Integer matricula, String curso, Integer periodo) {
+	//Ctrl +3 >>> gcuf cria construtor automatico 
+	public Aluno(String nome, Integer matricula, float notaAV1, float notaAV2, float notaAE, String curso,
+			Integer periodo) {
+		super();
 		this.nome = nome;
 		this.matricula = matricula;
-		this.curso = curso;
-		this.periodo = periodo;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public Integer getMatricula() {
-		return matricula;
-	}
-	public void setMatricula(Integer matricula) {
-		this.matricula = matricula;
-	}
-	public String getCurso() {
-		return curso;
-	}
-	public void setCurso(String curso) {
-		this.curso = curso;
-	}
-	public Integer getPeriodo() {
-		return periodo;
-	}
-	public void setPeriodo(Integer periodo) {
-		this.periodo = periodo;
-	}
-	public Double getNotaAV1() {
-		return notaAV1;
-	}
-	public void setNotaAV1(Double notaAV1) {
 		this.notaAV1 = notaAV1;
-	}
-	public Double getNotaAV2() {
-		return notaAV2;
-	}
-	public void setNotaAV2(Double notaAV2) {
 		this.notaAV2 = notaAV2;
-	}
-	public Double getNotaAE() {
-		return notaAE;
-	}
-	public void setNotaAE(Double notaAE) {
 		this.notaAE = notaAE;
+		this.curso = curso;
+		this.periodo = periodo;
 	}
 	
+	public void alteraNotaAV1 (float nota)
+	{
+		this.notaAV1 = nota;
+	}
 	
-	public String avaliarAluno(Double notaAV1, Double notaAV2) {
-		Double 	soma = notaAV1 + notaAV2; 
+	public void alteraNotaAV2 (float nota)
+	{
+		this.notaAV2 = nota;
+	}
 	
-	if (soma >= 60) 
+	public void alteraNotaAE (float nota)
+	{
+		this.notaAE = nota;
+	}
+	
+	public String avaliarAluno () {
+	if ((this.notaAV1 + this.notaAV2)>=60)
 		return "Aprovado";
 	else 
-		return "Reprovado";
+		return "Recuperação";
 	}
-	public String avaliarRecuperacao(Double notaAV1, Double notaAV2, Double notaAE) {
-		Double avaliacao = (notaAV1 + notaAV2 + notaAE) / 3;
 	
-	if (avaliacao >= 60)
+	
+	public String avaliarRecuperacao() {
+	if ((this.notaAV1 + this.notaAV2 + this.notaAE)/2 >= 60)
 		return "Aprovado";
 	else 
-		return "Reprovado";
-		
+		return "Reprovado";	
 	}
-	
-	
-}
 
-	
+
+}
