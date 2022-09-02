92f6a492dc0dfd1048b8458e9af8e6150b20160f
Bruna Zunzarren <104565697+Brunazunzarrenn@users.noreply.github.com>
2022-09-01 22:09:00 -0300

    Update alterarNota.java

--- Atividades/src/notasAlunos/alterarNota.java
+++ Atividades/src/notasAlunos/alterarNota.java
@@ -5,7 +5,11 @@ public class alterarNota {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner (System.in);
-		Aluno aluno1 = new Aluno ("Bruna", 12113935, 40, 30, 0, "ads", 3);
+		Aluno aluno1 = new Aluno ("Bruna", 12113935, 20, 30, 0, "ads", 3);
+		System.out.println(aluno1.avaliarAluno());
+		System.out.println(aluno1.avaliarRecuperacao());
+		aluno1.alteraNotaAE(70);
+		System.out.println("Alterar nota da AE");
 		System.out.println(aluno1.avaliarAluno());
 	}
 }
